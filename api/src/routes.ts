c5758f579944a9eb7ef99a80f2df45a71bf8a37b
luan.staner <luan.staner@gmail.com>
2021-02-02 15:43:20 -0300

    websocket in routes

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,11 +1,12 @@
-import express, { application } from "express";
+import express from "express";
 import { Socket } from "socket.io";
 import app from './config/servers/index';
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  const socket:Socket  = app.get("io");
-  console.log(socket);
+router.get("/:name/:nickname", (req, res) => {
+  const socket:Socket  = app.get("io")
+  const {name, nickname} = req.params
+  socket.emit("sendOthersUsers", {name ,  nickname })
   
   res.send({ response: "I am alive" }).status(200);
 });
