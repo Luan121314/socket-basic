774d1403c17064cb8cf619a608a396fac790fe19
luan.staner <luan.staner@gmail.com>
2021-02-02 15:44:39 -0300

    Added listening

--- reactsocket/src/components/clientSocket.tsx
+++ reactsocket/src/components/clientSocket.tsx
@@ -1,19 +1,31 @@
 import React, {  useMemo, useState } from 'react';
 import Socket from '../services/socket'
 
+interface UserProps {
+    name:string,
+    nickname: string
+}
+
 const ClientSocket = () => {
     const [response, setResponse] = useState("");
-  const socketConnect =  useMemo(()=>{
+    const [showNewUser, setNewShowNewUser] = useState<UserProps>();
+   useMemo(()=>{
         const socket = Socket();
         socket.on("FromAPI", (data: string) => {
             setResponse(data)
+        })
+
+        socket.on("sendOthersUsers", (data: UserProps) =>{
+            setNewShowNewUser(data)
         })
     },[])
 
 
     return (
         <div>
-            <h2>Contando : {response}</h2>
+            <h2>Cont : {response}</h2>
+            <h3>New user: {showNewUser?.name} <p>
+                </p> <strong>nickname: {showNewUser?.nickname}</strong></h3>
         </div>
     )
 }
